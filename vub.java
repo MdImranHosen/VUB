eb72077b9d96bab421d2361f6bc3e103e4f6d38e
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2019-10-27 19:44:37 +0600

    java first commit

---
+++ vub.java
@@ -0,0 +1,53 @@
+package mdimranhosen;
+
+/**
+ *
+ * @author MD IMRAN HOSEN
+ */
+public class MdImranHosen {
+  
+    public static void main(String[] array){
+        int num = 7;
+        if(num > 9){
+        System.out.println("The Number is "+num);
+        
+        }
+        for(int i=1; i<=10; i++){
+         System.out.println(i);
+        }
+    System.out.println("I'm MD IMRAN HOSEN");
+    
+    if(num%2==0){
+    System.out.println("Even Number");
+    } else{
+     System.out.println("Odd Number");
+    }
+    
+    
+    // Even Number Output is ...
+    System.out.print("Even Number is: ");
+    for(int m=1; m<=22; m++){
+     if(m%2==0){
+      System.out.print(" "+m);
+     }
+    }
+    System.out.println();
+    
+    
+  // Odd Number Output with Java.....
+ System.out.print("Odd Number is: ");
+    for(int f=1; f<=22; f++){
+       
+        if(f%2 != 0){
+         System.out.print(" "+f);
+        }
+    }
+   
+    System.out.println();
+    
+    
+    
+    }
+    
+          
+}

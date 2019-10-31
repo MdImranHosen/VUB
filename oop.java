49e24d0bc9b522a9b0235091ae1539b3c5f43322
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2019-10-31 19:52:45 +0600

    user input number to output prime number

--- oop.java
+++ oop.java
@@ -51,6 +51,50 @@ public class Imran {
     
    }
 }
+/////////////////:::::::::::::::::::: User input to Prime Number Output ::::::::::::::::::://///////////
+
+//Create Package name imranhosen
+package imranhosen;
+
+/**
+ *
+ * @author MD IMRAN HOSEN
+ */
+
+//Create File name Imran.java
+import java.util.Scanner;
+public class Imran {
+   public static void main(String arr[]){
+   
+       try {
+       int i, n, m=0, f=0;
+       Scanner obj = new Scanner(System.in);
+       System.out.print("Enter One Integer Number : ");
+       n = obj.nextInt();
+       m = n/2;
+       if(n==0 || n==1){
+       System.out.print(n+" is not Prime Number. \n");
+       } else{
+         for(i=2; i<=m; i++){
+          if(n%i==0){
+            System.out.print(n+" is not Prime Number. \n");
+            f=1;
+            break;
+          }
+         }
+       }
+       if(f==0){
+       System.out.print(n+" is Prime Number. \n");
+       }
+       
+       } catch(Exception e){
+        System.out.print("Only Allow Integer Number.\n");
+       }
+   
+   } 
+
+}
+
 /////::::::::::::::::: Date and Time :::::::::::::::::://///////////
 
 import java.time.*;

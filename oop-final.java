645390ce2176d6caf5444a10bfbc5211e22fb960
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2020-01-17 23:14:37 +0600

    Maximum and Minimum Number

--- oop-final.java
+++ oop-final.java
@@ -27,4 +27,39 @@ public class ImranCsit{
 
 }
 
-///////////////:::::::::::END - 1: User input number to Overloadin ( Method, this keyword use ) ::::::::::://///\\\\\\\\\\\\
+///////////////:::::::::::  2: User input number to Output Maximum and Minimum Number ::::::::::://///\\\\\\\\\\\\
+
+import java.util.Scanner;
+public class ImranCsit{
+
+    public static void main(String arr[]){
+        System.out.print("Enter intiger Number: ");
+        Scanner inputn = new Scanner(System.in);
+        int x = inputn.nextInt();
+        int y = inputn.nextInt();
+
+        ImranCsit obj = new ImranCsit();
+        
+        int mn = obj.myFunction(x,y);
+        
+        System.out.print("Maximum Number is "+obj.myFunction(x,y)+" \n"+
+                "Minimum Number is "+obj.myFunctionMin(x,y)+" \n");
+    }
+
+    public int myFunction(int a, int b) {
+        
+        if(a>b) {
+         return a;
+        } else{
+         return b;
+        }
+    }
+    public int myFunctionMin(int a, int b){
+      if(a>b){
+       return b;
+      } else{
+       return a;
+      }
+    }
+}
+///////////////////////::::::::::::::::::::: End :::::::::::::::::::::::::::::::::\\\\\\\\\\\\\\\\\\\\\\

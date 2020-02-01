9bb03fca256f85bce591b02a07aa2f0ba14a719c
Md. Imran Hosen <33275522+MdImranHosen@users.noreply.github.com>
2020-02-01 22:37:41 +0600

    ip address

--- imran.c
+++ imran.c
@@ -8,3 +8,23 @@ int main()
     printf("%d + %d = %d", num1, num2, total);
     return 0;
 }
+/////////////////////////////::::::::::::: IP Address ::::::::::\\\\\\\\\\\\\\\\\\\\\\
+#include<stdio.h>
+void main(){
+ int m1,m2,m3,m4;
+ printf("Enter the IP Address: ");
+ scanf("%d.%d.%d.%",&m1,&m2,&m3,&m4);
+ if(m1>=0&&m1<=127){
+    printf("\nGiven IP is in Class A");
+ } else if(m1>=128&&m1<=191){
+    printf("\nGiven IP is in Class B");
+  } else if(m1>=192&&m1<=223){
+    printf("\nGiven IP is in Class C");
+  } else if(m1>=224&&m1<=239){
+    printf("\nGiven IP is in Class D");
+  } else if(m1>=240&&m1<=255){
+    printf("\nGiven IP is in Class E");
+  }else{
+    printf("\n Error is Input");
+  }
+}
